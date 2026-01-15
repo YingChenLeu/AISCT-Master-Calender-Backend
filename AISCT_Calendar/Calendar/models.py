fd2935d3ff68ee701bca0fcb63960d5ac49008c5
Wiktor Waligora <wiktorwaligora@icloud.com>
2026-01-15 16:10:43 +0200

    added models

--- AISCT_Calendar/Calendar/models.py
+++ AISCT_Calendar/Calendar/models.py
@@ -2,11 +2,28 @@ from django.db import models
 
 # Create your models here.
 class User(models.Model):
-    username = models.CharField(max_length=150, unique=True)
+    name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254, unique=True)
+    grade = models.IntegerField()
+
 
     def __str__(self):
         return self.username
 class Event(models.Model):
     title=models.CharField(max_length=200)
+    location=models.CharField(max_length=50)
+    description=models.CharField(max_length=200)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
     def __str__(self):
         return self.title
+
+class Assignment(models.Model):
+    title=models.CharField(max=200)
+
+class Day(models.Model):
+    a_day = models.BooleanField(True)
+    half_day = models.BooleanField(True)
+    off_day = models.BooleanField(True)
+    def __str__(self):
+        return self.a_day
