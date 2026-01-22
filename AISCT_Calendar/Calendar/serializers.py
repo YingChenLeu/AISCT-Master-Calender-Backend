14e7bd99c63f73029c9b09a66b022ff83941f128
Wiktor Waligora <wiktorwaligora@icloud.com>
2026-01-22 15:28:34 +0200

    change to apps

--- AISCT_Calendar/Calendar/serializers.py
+++ AISCT_Calendar/Calendar/serializers.py
@@ -2,3 +2,5 @@ from rest_framework import serializers
 # from rest_framework.validators import UniqueValidator
 # from django.contrib.auth.models import User
 
+class EventSerializer(serializers.Serializer):
+    
--- AISCT_Calendar/mysite/settings.py
+++ AISCT_Calendar/mysite/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Calendar.apps.CalendarConfig',
-    'djangorestframework',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
